b0780b535078bba4528d3e582c323274900884c1
Deepeshh18 <ydeepesh682@gmail.com>
2023-04-17 23:21:32 +0530

    initial commit

---
+++ react_redux_shopping_cart/react_redux_shopping_cart/src/components/CardsData.js
@@ -0,0 +1,72 @@
+const Cardsdata = [
+  {
+    id: 1,
+    rname: "TRQ White Shoesyy",
+    imgdata:
+      "https://rukminim1.flixcart.com/image/300/300/l51d30w0/shoe/z/w/c/10-mrj1914-10-aadi-white-black-red-original-imagft9k9hydnfjp.jpeg?q=70",
+    address: "North Indian, Biryani, Mughlai",
+    delimg:
+      "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+    somedata: " 1175 + order placed from here recently",
+    price: 350,
+    rating: "3.8",
+    arrimg:
+      "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+    qnty: 0,
+  },
+  {
+    id: 2,
+    rname: "LOREM Watch Black",
+    imgdata:"https://5.imimg.com/data5/KC/PC/MY-38629861/dummy-chronograph-watch-500x500.jpg",
+    address: "Street Food",
+    somedata: " 2525 + order placed from here recently",
+    price: 125,
+    rating: "3.9",
+    qnty: 0,
+  },
+  {
+    id: 3,
+    rname: "AMZ Laptop 8GB RAM",
+    imgdata:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq39iB_cO6uhZ59vubrqVuYTJQH-4Qa0hU9g&usqp=CAU",
+    address: "Pizza, Fast Food, Pasta",
+    somedata: " 650 + order placed from here recently",
+    price: 570,
+    rating: "4.2",
+    qnty: 0,
+  },
+  {
+    id: 4,
+    rname: "Security Camera",
+    imgdata:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfvoDzLqrT7GwU3z7Ccp0Cl9rV0ZnU9DcmEg&usqp=CAU",
+    address: "Momos",
+    somedata: " 300 + order placed from here recently",
+    price: 270,
+    rating: "3.8",
+    qnty: 0,
+  },
+  {
+    id: 5,
+    rname: "Watch Pink",
+    imgdata:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG9e8Axt-h9q8EIybKfjGzbkIWJAr50_BX7Q&usqp=CAU",
+    address: "North Indian",
+    somedata: "1050 + order placed from here recently",
+    price: 210,
+    rating: "4.0",
+    qnty: 0,
+  },
+  {
+    id: 6,
+    rname: "Cup red Color",
+    imgdata:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9xzgtOpMxdpfgBOg3OKsEqYRkNBbuprJj4w&usqp=CAU",
+    address: "Wraps FastFood, Chines",
+    somedata: " 1100 + order placed from here recently",
+    price: 100,
+    rating: "3.8",
+    qnty: 0,
+  },
+];
+
+export default Cardsdata;
